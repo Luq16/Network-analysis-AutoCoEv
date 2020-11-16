6879bac4b68ca9b8a3019442e07eda3e4512f4e4
Luqman <luqawoniyi@gmail.com>
2020-11-16 11:23:48 +0200

    Add files via upload

---
+++ howToRun.R
@@ -0,0 +1,10 @@
+######################################################################################
+#How to use
+
+#Save this app.R script to a folder
+#open a new R Script
+#First define the location of xlsx file and name it filePath
+#runApp('location of app.R')
+
+filePath<-"C:/Users/luolaw/Desktop/AutoCoev/raftCoev/final/raft_0_00001.xlsx"
+runApp('C:/Users/luolaw/Desktop/AutoCoev/Shiny/Network_App')
